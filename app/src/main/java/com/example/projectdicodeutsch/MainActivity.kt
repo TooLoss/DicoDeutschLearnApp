8c6bf88cff8a972eb320d5f1bc3e63a574ed6d9b
TooLoss <biloulhaddadi@gmail.com>
2022-09-01 17:42:01 +0200

    Mise à jour de la liste de mot

--- app/src/main/java/com/example/projectdicodeutsch/MainActivity.kt
+++ app/src/main/java/com/example/projectdicodeutsch/MainActivity.kt
@@ -1,21 +1,72 @@
 package com.example.projectdicodeutsch
 
-import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
-import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
+import com.google.gson.annotations.SerializedName
+import okhttp3.OkHttpClient
+import java.io.InputStreamReader
+import java.net.URL
+import javax.net.ssl.HttpsURLConnection
+
 
 class MainActivity : AppCompatActivity() {
+
+    private val client = OkHttpClient()
+    private val apiResponse = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //APICallVersion().start()
     }
 
     val pageList = listOf(translate_search::class.java, Exercice_frenchtodeutsch::class.java, wordlist::class.java)
 
+    /*
+
+    private fun APICallVersion(): Thread
+    {
+        return Thread() {
+            val url = URL("https://api.github.com/repos/TooLoss/DicoDeutschLearnApp/releases")
+            val connection = url.openConnection() as HttpsURLConnection
+            if(connection.responseCode == 200) {
+                val inputSystem = connection.inputStream
+                val inputStreamReader = InputStreamReader(inputSystem, "UTF-8")
+                val gsonBuilder = GsonBuilder()
+                val gson = gsonBuilder.create()
+
+                val request: Array<TestCase> = gson.fromJson(inputStreamReader, Array<TestCase>::class.java)
+                //val request = Gson().fromJson(inputStreamReader, Request::class.java)
+                hasNewUpdate()
+                inputStreamReader.close()
+                inputSystem.close()
+            } else {
+                println("TEST")
+            }
+        }
+    }
+
+    */
+
+    private class TestCase {
+        @SerializedName("test")
+        private val field: String? = null
+    }
+
+    /*
+    private fun hasNewUpdate() {
+        runOnUiThread {
+            kotlin.run {
+            }
+            println(field)
+        }
+    }
+
+     */
     fun goToPage(v: View) {
         val tagString = v.tag.toString()
         val pageNumber = Integer.valueOf(tagString)
