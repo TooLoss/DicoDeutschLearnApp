30b9bb7a33aab1c829254db6a9db63725def55c1
TooLoss <biloulhaddadi@gmail.com>
2022-09-03 13:25:12 +0200

    Possibilité de cacher les traductions en Allemand dans le mode liste de mots

--- app/src/main/java/com/example/projectdicodeutsch/wordlist.java
+++ app/src/main/java/com/example/projectdicodeutsch/wordlist.java
@@ -4,10 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.Activity;
+import android.content.res.ColorStateList;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.view.Window;
+import android.widget.CompoundButton;
+import android.widget.Toast;
+import android.widget.ToggleButton;
 
 import com.example.projectdicodeutsch.adapter.WordAdapter;
 
@@ -17,6 +22,7 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class wordlist extends AppCompatActivity {
@@ -26,6 +32,11 @@ public class wordlist extends AppCompatActivity {
 
     private RecyclerView wordRecyclerView;
     private WordAdapter wordAdapter;
+
+    private ToggleButton hideTranslationSwitch;
+    boolean hideTranslaiton = false;
+
+    Activity thisActvity = this;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,6 +54,31 @@ public class wordlist extends AppCompatActivity {
 
         wordAdapter = new WordAdapter(this, frenchListComplete, germanListComplete, true);
         wordRecyclerView.setAdapter(wordAdapter);
+
+        hideTranslationSwitch = findViewById(R.id.hideTranslationSwitch);
+        hideTranslationSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                int colorInt;
+                ColorStateList csl;
+                if(isChecked)  {
+                    colorInt = getResources().getColor(R.color.white);
+                    csl = ColorStateList.valueOf(colorInt);
+                    hideTranslationSwitch.setBackgroundTintList(csl);
+
+                    List<String> emptyList = Arrays.asList(new String[frenchListComplete.size() - 1]);
+                    wordAdapter = new WordAdapter(thisActvity, frenchListComplete, emptyList, true);
+                    wordRecyclerView.setAdapter(wordAdapter);
+                } else {
+                    colorInt = getResources().getColor(R.color.primary);
+                    csl = ColorStateList.valueOf(colorInt);
+                    hideTranslationSwitch.setBackgroundTintList(csl);
+
+                    wordAdapter = new WordAdapter(thisActvity, frenchListComplete, germanListComplete, true);
+                    wordRecyclerView.setAdapter(wordAdapter);
+                }
+            }
+        });
     }
 
     private void generateCsvList() throws IOException {
